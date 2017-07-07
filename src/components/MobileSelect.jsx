bd1729b4e3ff0cfc21ef708a7b521e9d1a9be45f
tianxiangbing <55342775@qq.com>
2017-07-07 15:32:02 +0800

    first commit

---
+++ src/components/MobileSelect.jsx
@@ -0,0 +1,20 @@
+/*
+ * Created with Visual Studio Code.
+ * github: https://github.com/React-Plugin/x-seed
+ * User: 田想兵
+ * Date: 2017-05-14
+ * Time: 20:00:00
+ * Contact: 55342775@qq.com
+ */
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class MobileSelect extends Component {
+  render () {
+    return (
+      <div className="MobileSelect">
+        hello world...<a href="https://github.com/React-Plugin/x-seed">https://github.com/React-Plugin/x-seed</a>
+      </div>
+    );
+  }
+}
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+/*
+ * Created with Visual Studio Code.
+ * github: https://github.com/React-Plugin/x-MobileSelect
+ * User: 田想兵
+ * Date: 2017-07-07
+ * Time: 20:00:00
+ * Contact: 55342775@qq.com
+ */
+import MobileSelect from './components/MobileSelect';
+module.exports = MobileSelect;  //使用module.exports时，从es6到es5生成的dist不会出现export.default的问题.
